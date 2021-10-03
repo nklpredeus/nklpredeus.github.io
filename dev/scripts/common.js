8e58f1fe9d107b62f5b2c43ba603fc42dd6d1bc6
nklpredeus <nkl.predeus@gmail.com>
2021-10-03 13:45:04 +0300

    button js

--- dev/scripts/common.js
+++ dev/scripts/common.js
@@ -16,3 +16,17 @@ $(document).mouseup(function (e) {
         $('.hamburger').removeClass('is-active');
     }
 });
+
+
+ $(function() {
+ $(window).scroll(function() {
+ if($(this).scrollTop() != 0) {
+ $('#toTop').fadeIn();
+ } else {
+ $('#toTop').fadeOut();
+ }
+ });
+ $('#toTop').click(function() {
+ $('body,html').animate({scrollTop:0},800);
+ });
+ });
