1cc2f08714c36984e16e14149183137888ab5be0
nklpredeus <nkl.predeus@gmail.com>
2021-10-14 11:57:33 +0300

    new folder

--- StartUp/dev/scripts/common.js
+++ StartUp/dev/scripts/common.js
@@ -36,8 +36,8 @@ $(document).ready(function () {
 		infinite: true,
 		arrows: true,
 		dots: false,
-		prevArrow: '<img class="team-arrow-left" src="/images/fa-angle-left.png" alt="left"> ',
-		nextArrow: '<img class="team-arrow-right" src="/images/fa-angle-right.png" alt="right">',
+		prevArrow: '<img class="team-arrow-left" src="images/fa-angle-left.png" alt="left"> ',
+		nextArrow: '<img class="team-arrow-right" src="images/fa-angle-right.png" alt="right">',
 		autoplay: true,
 		autoSpeedplay: 30,
 	});
@@ -47,12 +47,11 @@ $(document).ready(function () {
 		infinite: true,
 		arrows: true,
 		dots: false,
-		prevArrow: '<img class="brand-arrow-left" src="/images/fa-angle-left.png" alt="left"> ',
-		nextArrow: '<img class="brand-arrow-right" src="/images/fa-angle-right.png" alt="right">',
+		prevArrow: '<img class="brand-arrow-left" src="images/fa-angle-left.png" alt="left"> ',
+		nextArrow: '<img class="brand-arrow-right" src="images/fa-angle-right.png" alt="right">',
 		autoplay: true,
 		autoSpeedplay: 30,
 	});
-
 });
 
 
