587183ecd9b5ea8ed062f1cbfa5b76f193905649
nklpredeus <nkl.predeus@gmail.com>
2021-10-14 11:42:39 +0300

    arrows

--- dev/scripts/common.js
+++ dev/scripts/common.js
@@ -36,10 +36,10 @@ $(document).ready(function () {
 		infinite: true,
 		arrows: true,
 		dots: false,
-		prevArrow: '<img class="team-arrow-left" src="../images/fa-angle-left.png" alt="left"> ',
-		nextArrow: '<img class="team-arrow-right" src="../images/fa-angle-right.png" alt="right">',
+		prevArrow: '<img class="team-arrow-left" src="/images/fa-angle-left.png" alt="left"> ',
+		nextArrow: '<img class="team-arrow-right" src="/images/fa-angle-right.png" alt="right">',
 		autoplay: true,
-		autoSpeedplay: 10,
+		autoSpeedplay: 30,
 	});
 
 	$('.brand-slider').slick({
@@ -47,8 +47,10 @@ $(document).ready(function () {
 		infinite: true,
 		arrows: true,
 		dots: false,
-		prevArrow: '<img class="brand-arrow-left" src="../images/fa-angle-left.png" alt="left"> ',
-		nextArrow: '<img class="brand-arrow-right" src="../images/fa-angle-right.png" alt="right">',
+		prevArrow: '<img class="brand-arrow-left" src="/images/fa-angle-left.png" alt="left"> ',
+		nextArrow: '<img class="brand-arrow-right" src="/images/fa-angle-right.png" alt="right">',
+		autoplay: true,
+		autoSpeedplay: 30,
 
 	});
 
