091ffc577203398786e38f494f137b72aa237500
nklpredeus <nkl.predeus@gmail.com>
2021-10-21 22:20:31 +0300

    new mixer + slide

--- boostfolia/dev/scripts/common.js
+++ boostfolia/dev/scripts/common.js
@@ -16,6 +16,18 @@ $(document).ready(function () {
 	// });
 
 
+	$('.black-carousel-list').slick({
+		slidesToShow: 3,
+		infinite: true,
+		arrows: true,
+		dots: false,
+		prevArrow: '<img class="carousel-arrow-left" src="images/fa-angle-left.png" alt="left"> ',
+		nextArrow: '<img class="carousel-arrow-right" src="images/fa-angle-right.png" alt="right">',
+		autoplay: true,
+		autoSpeedplay: 30,
+	});
+
+
 	var mixer = mixitup('.work-items');
 
 });
