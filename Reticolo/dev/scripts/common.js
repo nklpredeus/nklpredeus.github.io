2c80a074cd63f5e5593ab1bb2c80b1c2df61f00b
nklpredeus <nkl.predeus@gmail.com>
2021-10-28 21:43:37 +0300

    add reticolo

---
+++ Reticolo/dev/scripts/common.js
@@ -0,0 +1,118 @@
+$(document).ready(function () {
+
+	$('.hamburger').click(function () {
+		$('.hamburger').toggleClass('is-active');
+		$('.mobile-menu').toggleClass('active');
+	});
+
+	$('.close').click(function () {
+		$('.mobile-menu').removeClass('active');
+		$('.hamburger').removeClass('is-active');
+	})
+
+	$(document).mouseup(function (e) {
+		var menu = $('.mobile-menu')
+		if (!menu.is(e.target)) {
+			menu.removeClass('active');
+			$('.hamburger').removeClass('is-active');
+		}
+	});
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() != 0) {
+			$('#toTop').fadeIn();
+		} else {
+			$('#toTop').fadeOut();
+		}
+	});
+	$('#toTop').click(function () {
+		$('body,html').animate({ scrollTop: 0 }, 800);
+	});
+
+	//Team Slider
+
+	$('.team-slider').slick({
+		slidesToShow: 4,
+		infinite: true,
+		arrows: true,
+		dots: false,
+		prevArrow: '<img class="team-arrow-left" src="images/fa-angle-left.png" alt="left"> ',
+		nextArrow: '<img class="team-arrow-right" src="images/fa-angle-right.png" alt="right">',
+		autoplay: true,
+		autoSpeedplay: 30,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+					autoplay: false
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					autoplay: true,
+					arrows: false
+				}
+			},
+			{
+				breakpoint: 575,
+				settings: {
+					slidesToShow: 1,
+					autoplay: true,
+					arrows: false
+				}
+			}
+		]
+	});
+
+	$('.brand-slider').slick({
+		slidesToShow: 5,
+		infinite: true,
+		arrows: true,
+		dots: false,
+		prevArrow: '<img class="brand-arrow-left" src="images/fa-angle-left.png" alt="left"> ',
+		nextArrow: '<img class="brand-arrow-right" src="images/fa-angle-right.png" alt="right">',
+		autoplay: true,
+		autoSpeedplay: 30,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 4,
+					autoplay: true
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 3,
+					autoplay: true,
+					arrows: false
+				}
+			},
+			{
+				breakpoint: 575,
+				settings: {
+					slidesToShow: 2,
+					autoplay: true,
+					arrows: false
+				}
+			},
+			{
+				breakpoint: 415,
+				settings: {
+					slidesToShow: 1,
+					autoplay: true,
+					arrows: false
+				}
+			},
+		]
+	});
+
+	var mixer = mixitup('.latest-works-mixer');
+
+});
+
+
