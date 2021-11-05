86926df0a7123fad6a86103f0b690c5295c7bdef
nklpredeus <nkl.predeus@gmail.com>
2021-11-05 19:34:28 +0200

    all

--- present-poland/dev/scripts/common.js
+++ present-poland/dev/scripts/common.js
@@ -18,48 +18,6 @@ $(document).ready(function () {
 		}
 	});
 
-
-	//Team Slider
-
-	$('.team-slider').slick({
-		slidesToShow: 4,
-		infinite: true,
-		arrows: true,
-		dots: false,
-		prevArrow: '<img class="team-arrow-left" src="images/fa-angle-left.png" alt="left"> ',
-		nextArrow: '<img class="team-arrow-right" src="images/fa-angle-right.png" alt="right">',
-		autoplay: true,
-		autoSpeedplay: 30,
-		responsive: [
-			{
-				breakpoint: 992,
-				settings: {
-					slidesToShow: 3,
-					autoplay: false
-				}
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 2,
-					autoplay: true,
-					arrows: false
-				}
-			},
-			{
-				breakpoint: 575,
-				settings: {
-					slidesToShow: 1,
-					autoplay: true,
-					arrows: false
-				}
-			}
-		]
-	});
-
-
-	var mixer = mixitup('.latest-works-mixer');
-
 });
 
 
